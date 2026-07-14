3fbffc9ad527ab89e1d33f85511634c4738af271
Minecraft3193092 <97770678+Minecraft3193092@users.noreply.github.com>
2026-07-14 21:48:34 +0800

    ok

--- anime/anime-8bit/new/painting-sandbox/software/php/wiki/resources/views/wiki.blade.php
+++ anime/anime-8bit/new/painting-sandbox/software/php/wiki/resources/views/wiki.blade.php
@@ -6,6 +6,10 @@
 </head>
 
 <style>
+.title-s {
+    border-bottom: 2px solid #a2a9b1;
+}
+
 .wrapper {
     display: flex;
     gap: 20px;
@@ -14,30 +18,59 @@
 }
 
 .content {
-    flex: 3;
-    background-color: #ffffff;
+    flex: 2;
+    height: 100%;
+    border-radius: 8px;
+    background-color: #f4f4f4;
     padding: 20px;
 }
 
 .sidebar {
     flex: 1;
-    background-color: #f4f4f4;
+    border-radius: 8px;
+    background-color: #e5e5e5;
     padding: 20px;
+}
+
+.border-2 {
+    padding: 10px;
+}
+
+.header-text-container {
+  text-align: center; 
+  border-radius: 8px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+.footer-text-container {
+  text-align: center; 
+  border-radius: 8px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+.nav {
+  text-align: center; 
+  padding: 10px;
 }
 </style>
 
 <body>
 
 <header>
-<nav>
-  <a href="/mainpage/">{{ __('msg.mainpage') }}</a> | {{ __('msg.welcome', ['name' => $userData->name]) }} | <a href="/login/">{{ __('msg.login') }}</a>
+<div class="text-container">
+ Wiki
+</div>
+<nav class="nav">
+<a href="/mainpage/">{{ __('msg.mainpage') }}</a> | {{ __('msg.welcome', ['name' => $userData->name]) }} | <a href="/login/">{{ __('msg.login') }}</a>
 </nav>
 </header>
 
 <div class="wrapper">
 <main class="content">
 <article>
-<h1>{{ $page['page_title'] }}</h1>
+<h1 class="title-s">{{ $page['page_title'] }}</h1>
 
 {{ $page['page_content'] }}
 </article>
@@ -49,5 +82,10 @@
 
 </div>
 <footer>
+<div class="border-2">
+<div class="footer-text-container">
+ Wiki
+</div>
+</div>
 </footer>
 </body>
